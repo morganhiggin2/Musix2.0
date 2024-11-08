c9190a9ec77ae58214a13e98a73ede892d6ea48f
morganhiggin2 <morganhigginbotham2@gmail.com>
2024-11-07 19:15:53 -0700

    remove now unnecessary settings

--- src/settings_parser.rs
+++ src/settings_parser.rs
@@ -1,48 +1,45 @@
-use std::fs::File;
-use std::io::prelude::*;
 use getset::Getters;
 use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::prelude::*;
 
-#[derive(Serialize, Deserialize)]
-#[derive(Getters)]
+#[derive(Serialize, Deserialize, Getters)]
 pub struct Settings {
-    #[getset(get = "pub")]
-    playlists: Vec<PlaylistSetting>
+    //#[getset(get = "pub")]
 }
 
-#[derive(Serialize, Deserialize)]
-#[derive(Getters)]
-pub struct PlaylistSetting {
-    #[getset(get = "pub")]
-    id: String,
-
-    #[getset(get = "pub")]
-    genre: String
-}
-
-pub fn parse_settings() -> Result<Settings, String>{
+pub fn parse_settings() -> Result<Settings, String> {
     //open settings file
     let mut file = match File::open("../settings/settings.json") {
-        Ok(some) => some, 
+        Ok(some) => some,
         Err(e) => {
-            return Err(format!("Error opening file \"../settings/settings.json\": {}", e))
+            return Err(format!(
+                "Error opening file \"../settings/settings.json\": {}",
+                e
+            ))
         }
-    }; 
+    };
 
     //read file as one string
     let mut contents = String::new();
     match file.read_to_string(&mut contents) {
         Ok(_size) => (),
         Err(e) => {
-            return Err(format!("Error reading contents of settings.json file: {}", e));
-        } 
+            return Err(format!(
+                "Error reading contents of settings.json file: {}",
+                e
+            ));
+        }
     };
 
     //parse file contents as json
     let parsed_settings: Settings = match serde_json::from_str(&contents) {
         Ok(some) => some,
         Err(e) => {
-            return Err(format!("Error parsing settings.json file contents as json with binding schema: {}", e));
+            return Err(format!(
+                "Error parsing settings.json file contents as json with binding schema: {}",
+                e
+            ));
         }
     };
 
