c37c4ec018df654f47c299daa8ba30cdd9f4b68f
morganhiggin2 <morganhigginbotham2@gmail.com>
2025-01-16 20:23:24 -0800

    scoping out the s3 service

--- src/s3_service.rs
+++ src/s3_service.rs
@@ -1,10 +1,34 @@
+use aws_config::{self, meta::region::RegionProviderChain, BehaviorVersion};
+use aws_sdk_s3;
 use std::path::Path;
 
 // Get the file from s3 and write into the desired location
-pub fn fetch_file_from_s3(s3_path: String, local_write_path: &Path) -> Result<(), String> {
-    // Get the config for the aws sdk
-    // Read the file
-}))
+pub async fn fetch_file_from_s3(s3_path: String, local_write_path: &Path) -> Result<(), String> {
+    // Extract bucket and key from s3 path
+
+    //TODO is it loading the aws config from the local place?
+    let region_provider = RegionProviderChain::default_provider().or_else("us-west-1");
+    let config = aws_config::defaults(BehaviorVersion::v2024_03_28())
+        .region(region_provider)
+        .load()
+        .await;
+
+    let client = aws_sdk_s3::Client::from_conf(config);
+
+    // TODO write file to local path
+    let object = client
+        .get_object()
+        .bucket("my_bucket")
+        .key("my_key")
+        .send()
+        .await?;
+
+    let body = object.body().await?;
+
+    // Write the file to the local path
+
+    return Ok(());
+}
 
 // Write file into the desired s3 location from the desired location
 
